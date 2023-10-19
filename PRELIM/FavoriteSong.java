579bff07febb3c35ccb03da5182fc43fd9d583d5
unknown <OO301@ccs.local>
2023-10-19 18:21:47 +0100

    added files

---
+++ PRELIM/FavoriteSong.java
@@ -0,0 +1,18 @@
+public class FavoriteSong{
+   static String[] song = {"Ah basta gusto lang namang magpalutang-lutang", "Magpatangay sa banayad na ihip ng hangin", "Nakakapagod ang puro pangangamba", "Kailangan ko yatang matutong huminga"};
+   
+   public static void main(String[] args){
+      FavoriteSong lyrics  = new FavoriteSong();
+      for (String l : lyrics.song) System.out.println(l);
+   } 
+}
+
+// class NotsoFavoriteSong{
+//    public void MyRoutinesong(){
+//       System.out.Print("Ah basta gusto lang namang magpalutang-lutang");
+//       System.out.Println("Magpatangay sa banayad na ihip ng hangin");
+//       System.out.Print("Nakakapagod ang puro pangangamba");
+//       System.out.Print("Kailangan ko yatang matutong huminga");
+// 
+//    }
+// }
